950d38d15806167679e292d387d95031478bbcfe
Anton <forsakenanton@gmail.com>
2023-04-27 19:01:19 +0300

    Add Method divide

--- GitCalculatorExample/GitCalculatorExample.cpp
+++ GitCalculatorExample/GitCalculatorExample.cpp
@@ -17,6 +17,11 @@ float multiple(float a, float b)
     return a * b;
 }
 
+float divide(float a, float b)
+{
+    return a / b;
+}
+
 int main()
 {
     cout << "Hello from the simple c++ calculator!\n";
