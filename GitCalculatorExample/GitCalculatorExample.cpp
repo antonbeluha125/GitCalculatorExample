9404c77999bc3552b6408a58178ead03652a6369
Anton <forsakenanton@gmail.com>
2023-04-27 19:01:19 +0300

    Add Method divide (add is b == 0)

--- GitCalculatorExample/GitCalculatorExample.cpp
+++ GitCalculatorExample/GitCalculatorExample.cpp
@@ -17,6 +17,16 @@ float multiple(float a, float b)
     return a * b;
 }
 
+float divide(float a, float b)
+{
+    if (b == 0)
+    {
+        return 0;
+    }
+
+    return a / b;
+}
+
 int main()
 {
     cout << "Hello from the simple c++ calculator!\n";
