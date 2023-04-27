dc743e24fb269b376883da907b05448298b7243c
Anton <forsakenanton@gmail.com>
2023-04-27 20:01:01 +0300

    Add isZero

--- GitCalculatorExample/GitCalculatorExample.cpp
+++ GitCalculatorExample/GitCalculatorExample.cpp
@@ -27,6 +27,21 @@ float divideMethod(float a, float b)
     return a / b;
 }
 
+float getMax(float a, float b)
+{
+    return a > b ? a : b;
+}
+
+float getMin(float a, float b)
+{
+    return a < b ? a : b;
+}
+
+bool isZero(float a)
+{
+    return a == 0 ? true : false;
+}
+
 int main()
 {
     cout << "Hello from the simple c++ calculator!\n";
